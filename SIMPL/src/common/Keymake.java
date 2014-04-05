51cd1c5b2cac1f63d5043cacb9c7a00576a0bff2
syreal17 <syreal14@yahoo.com>
2014-04-05 19:26:34 -0400

    Finished Keymake class. Manages PublicKey and PrivateKey operations between Client and Server

--- SIMPL/src/common/Keymake.java
+++ SIMPL/src/common/Keymake.java
@@ -3,7 +3,7 @@ package common;
 import java.io.*;
 import java.security.*;
 
-public class keymake
+public class Keymake
 {
 	private static boolean generated = false;
 	private static PrivateKey privateKey = null;
@@ -12,22 +12,14 @@ public class keymake
     private static void generatePair() throws UnsupportedOperationException {
 		try
 		{
-			if( !keymake.generated ){
+			if( !Keymake.generated ){
 				KeyPairGenerator kg = KeyPairGenerator.getInstance(Constants.ASYMMETRIC_CRYPTO_MODE);
 				kg.initialize(Constants.RSA_BITS);
 				KeyPair pair = kg.generateKeyPair();
-				keymake.privateKey = pair.getPrivate();
-				keymake.publicKey = pair.getPublic();
+				Keymake.privateKey = pair.getPrivate();
+				Keymake.publicKey = pair.getPublic();
 				
-				keymake.generated = true;
-				
-				/*Not writing the keys out in generateMethod
-				String fl = args[0];
-				FileOutputStream out1 = new FileOutputStream(fl);
-				byte[] ky1 = priKey.getEncoded();
-				out1.write(ky1);
-				out1.close();
-				*/
+				Keymake.generated = true;
 			} else {
 				throw new UnsupportedOperationException();
 			}
@@ -40,8 +32,9 @@ public class keymake
     
     public static void writePublicKey(String path){
     	try{
+    		Keymake.ensureGeneration();
 			FileOutputStream fos = new FileOutputStream(path);
-			byte[] publicKeyBytes = keymake.publicKey.getEncoded();
+			byte[] publicKeyBytes = Keymake.publicKey.getEncoded();
 			fos.write(publicKeyBytes);
 			fos.close();
     	} catch (FileNotFoundException e){
@@ -56,27 +49,41 @@ public class keymake
     }
     
     public static void writePrivateKey(String path){
-    	
+    	try{
+    		Keymake.ensureGeneration();
+			FileOutputStream fos = new FileOutputStream(path);
+			byte[] privateKeyBytes = Keymake.privateKey.getEncoded();
+			fos.write(privateKeyBytes);
+			fos.close();
+    	} catch (FileNotFoundException e){
+    		System.err.println(e.getMessage());
+    		e.printStackTrace();
+    		return;
+    	} catch (IOException e){
+    		System.err.println(e.getMessage());
+    		e.printStackTrace();
+    		return;
+    	}
     }
     
     public static PublicKey getPublicKey(){
-    	keymake.ensureGeneration();
-    	return keymake.publicKey;
+    	Keymake.ensureGeneration();
+    	return Keymake.publicKey;
     }
     
     public static PrivateKey getPrivateKey(){
-    	keymake.ensureGeneration();
-    	return keymake.privateKey;
+    	Keymake.ensureGeneration();
+    	return Keymake.privateKey;
     }
     
     /**
      * uses isGenerated and checkSanity to generate pair if it has not already been generated.
      */
     private static void ensureGeneration(){
-    	if( keymake.isGenerated() ){
+    	if( Keymake.isGenerated() ){
     		return;
     	} else {
-    		keymake.generatePair();
+    		Keymake.generatePair();
     	}
     }
     
@@ -86,7 +93,7 @@ public class keymake
      */
     private static boolean isGenerated(){
     	if( checkSanity() ){
-    		return keymake.generated;
+    		return Keymake.generated;
     	} else {
     		return false;
     	}
@@ -96,8 +103,8 @@ public class keymake
      * Ensures that nothing crazy happens like only one key existing and the other not
      */
     private static boolean checkSanity(){
-    	if( (keymake.privateKey == null && keymake.publicKey != null) ||
-    			keymake.privateKey != null && keymake.publicKey == null){
+    	if( (Keymake.privateKey == null && Keymake.publicKey != null) ||
+    			Keymake.privateKey != null && Keymake.publicKey == null){
     		throw new UnsupportedOperationException(Constants.KEY_PAIR_IMBALANCE_MSG);
     	}
     	
