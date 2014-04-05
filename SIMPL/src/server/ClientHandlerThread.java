20a75faf1aa42af38662ed060fed21343d8f1ccf
syreal17 <syreal14@yahoo.com>
2014-04-05 19:14:42 -0400

    Avoided null pointers like the plague

--- SIMPL/src/server/ClientHandlerThread.java
+++ SIMPL/src/server/ClientHandlerThread.java
@@ -18,13 +18,13 @@ public class ClientHandlerThread implements Runnable {
 		try{
 			Object o;
 			
+			//remember some variables for thread lifetime
+			this.server = CmdLine.server;
+			//query the Server's ClientHandler for the unhandled client socket
+			this.clientSocket = this.server.getClientHandler().getUnhandledEntry().getClientSocket();
+			this.clientStream = this.clientSocket.getInputStream();
+			
 			while( true ){
-				//remember some variables for thread lifetime
-				this.server = CmdLine.server;
-				//query the Server's ClientHandler for the unhandled client socket
-				this.clientSocket = this.server.getClientHandler().getUnhandledEntry().getClientSocket();
-				this.clientStream = this.clientSocket.getInputStream();
-							
 				//not doing FSM server side for beginning of comm; rather, relying on flags
 				byte[] recv = new byte[common.Constants.MAX_EXPECTED_PACKET_SIZE];
 				//wait for data from client
