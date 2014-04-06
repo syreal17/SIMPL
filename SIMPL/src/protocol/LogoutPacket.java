25fd7dc4ac01d4d5b702f7c1d71240a8a586c363
NeutronStar <jaffe.lu@husky.neu.edu>
2014-04-06 17:08:14 -0400

    Add logout packet

--- SIMPL/src/protocol/LogoutPacket.java
+++ SIMPL/src/protocol/LogoutPacket.java
@@ -1,6 +1,64 @@
 package protocol;
 
+import java.util.EnumSet;
+
 
 public class LogoutPacket extends ClientServerSessionPacket {
 
+	private static final long serialVersionUID = -5843876932465216286L;
+
+	/**
+	 * Client requests logout
+	 */
+	public void readyClientLogoutFIN(){
+		//reset the packet
+		this.clearAllFields();
+		
+		//prepare it as a Client Login request
+		this.setClientLogoutFINFlags();
+	}
+	
+	/**
+	 * Server says ok to logout
+	 */
+	public void readyServerLogoutFINACK(){
+		//reset the packet
+		this.clearAllFields();
+		
+		//prepare it as a Client Login request
+		this.setServerLogoutFINACKFlags();
+	}
+	
+	/**
+	 * Client says logging out
+	 */
+	public void readyClientLogoutACK(){
+		//reset the packet
+		this.clearAllFields();
+		
+		//prepare it as a Client Login request
+		this.setClientLogoutACKFlags();
+	}
+	
+	/**
+	 * Client logout FIN flag set
+	 */
+	private void setClientLogoutFINFlags(){
+		this.flags = EnumSet.of(Packet.Flag.Logout, Packet.Flag.Syncronization);
+	}
+	
+	/**
+	 * Server logout FIN/ACK flag set
+	 */
+	private void setServerLogoutFINACKFlags(){
+		this.flags = EnumSet.of(Packet.Flag.Logout, Packet.Flag.Syncronization, Packet.Flag.Acknowledgement);
+	}
+	
+	/**
+	 * Client logout ACK flag set
+	 */
+	private void setClientLogoutACKFlags(){
+		this.flags = EnumSet.of(Packet.Flag.Logout, Packet.Flag.Acknowledgement);
+	}
+	
 }
