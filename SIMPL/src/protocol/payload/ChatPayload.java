a3f8d533969d64167cf5501ecb594dc1917e1b0b
NeutronStar <jaffe.lu@husky.neu.edu>
2014-04-07 21:38:26 -0400

    Chat Payload complete

--- SIMPL/src/protocol/payload/ChatPayload.java
+++ SIMPL/src/protocol/payload/ChatPayload.java
@@ -1,5 +1,15 @@
 package protocol.payload;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
 import common.*;
 
 //TODO: Might even extend ClientServerSession if DH key can be made into byte[]
@@ -17,6 +27,56 @@ public class ChatPayload extends Payload {
 	public void copyFrom(Payload template) throws SimplException {
 		// TODO Auto-generated method stub
 		throw new UnsupportedOperationException(common.Constants.USO_EXCPT_MSG);
+	}
+	
+	public void encrypt(PublicKey buddyKey)
+	{
+		/* Encrypt the message with the symmetric key */
+		try {
+			Cipher cipher = Cipher.getInstance(common.Constants.SYMMETRIC_CRYPTO_MODE);
+			cipher.init(Cipher.ENCRYPT_MODE, buddyKey);
+			message = cipher.doFinal(message.getBytes()).toString();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void decrypt(PrivateKey myKey)
+	{
+		/* Encrypt the message with the symmetric key */
+		try {
+			Cipher cipher = Cipher.getInstance(common.Constants.SYMMETRIC_CRYPTO_MODE);
+			cipher.init(Cipher.DECRYPT_MODE, myKey);
+			message = cipher.doFinal(message.getBytes()).toString();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
